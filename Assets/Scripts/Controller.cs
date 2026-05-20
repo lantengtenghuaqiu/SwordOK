e0b6b0eb97da662fba9f4a637df2fa331b0fbfab
lantengtenghuaqiu <m13953332167@163.com>
2026-05-20 08:42:01 +0800

    Using RB button

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -15,7 +15,7 @@ public class Controller : MonoBehaviour
 
     public float LeftRocker_H;
     float LeftRocker_V;
-
+    bool buttonPressed;
     float RightRocker_H;
     float RightRocker_V;
 
@@ -25,28 +25,21 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Transform t = this.transform.parent;
-        //Debug.Log(t.childCount);
         UnityEngine.GameObject[] s = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
-
 
         for (int i = 0; i < s.Length; i++)
         {
-            Debug.Log(s[i].name);
             if (s[i].name == "RootManager")
                 character_entity = s[i].transform.GetChild(0).gameObject;
         }
 
         character_animator = character_entity.GetComponent<Animator>();
         if (character_animator == null)
-        {
             Debug.Log("null animator");
-        }
         else
-        {
             Debug.Log(character_animator.name);
 
-        }
+        StartCoroutine(Print(buttonPressed));
     }
 
     // Update is called once per frame
@@ -56,28 +49,26 @@ public class Controller : MonoBehaviour
         LeftRocker_V = Input.GetAxis("Vertical");
         RightRocker_H = Input.GetAxis("RightHorizontal");
         RightRocker_V = Input.GetAxis("RightVertical");
-        Debug.Log(RightRocker_H + " : " + RightRocker_V);
-        //Debug.Log(Math.Sqrt(Math.Pow(RightRocker_H, 2) + Math.Pow(RightRocker_V, 2)));
-        if (RightRocker_H > 0.5)
-        {
-            //first_input[0] = RightRocker_H;
-            //first_input[1] = RightRocker_V;
-            character_animator.SetBool("LeftAttack", true);
-        }
-        else
-        {
-            character_animator.SetBool("LeftAttack", false);
-        }
-        if (RightRocker_H < -0.5)
-        {
-            //first_input[0] = RightRocker_H;
-            //first_input[1] = RightRocker_V;
-            character_animator.SetBool("RightAttack", true);
-        }
-        else
-        {
-            character_animator.SetBool("RightAttack", false);
 
-        }
+        buttonPressed = Input.GetKey(KeyCode.JoystickButton5);
+        if (buttonPressed && RightRocker_H > 0.5)
+            character_animator.SetBool("LeftAttack", true);
+        else
+            character_animator.SetBool("LeftAttack", false);
+
+        if (buttonPressed && RightRocker_H < -0.5)
+            character_animator.SetBool("RightAttack", true);
+        else
+            character_animator.SetBool("RightAttack", false);
+    }
+
+    IEnumerator Print(bool buttonPressed)
+    {
+        yield return new WaitForSeconds(1f);
+        Debug.Log(RightRocker_H + " : " + RightRocker_V);
+        Debug.Log(buttonPressed ? "Button is pressing" : "Button is not pressed");
+
     }
 }
+
+
