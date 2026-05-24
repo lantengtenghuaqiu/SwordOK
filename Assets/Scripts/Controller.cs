a06fa5a890f363eb6f2dc0d4c5df264acdef5182
lantengtenghuaqiu <m13953332167@163.com>
2026-05-24 18:27:01 +0800

    Character Rotation

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Diagnostics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -28,55 +26,82 @@ public class Controller : MonoBehaviour
 
     Vector3 currentDirection;
     Vector3 rightController;
+
+    public float currentAngle = 0f;   // 当前角度
+    public float targetAngle = 0f;   // 目标角度
+    public float rotateSpeed = 0.08f; // 旋转速度
+    public float threshold = 0.1f;    // 到达阈值
+
     // Start is called before the first frame update
     void Start()
     {
         UnityEngine.GameObject[] s = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
         this.currentDirection = this.transform.forward;
-        //for (int i = 0; i < s.Length; i++)
-        //{
-        //    if (s[i].name == "RootManager")
-        //        character_entity = s[i].transform.GetChild(0).gameObject;
-        //}
+    }
+    private float ClampAngle(float angle)
+    {
+        angle = Mathf.Repeat(angle, 360f);
+        if (angle < 0)
+            angle += 360f;
+        return angle;
+    }
+    void CalculateAnagle()
+    {
+        if (Mathf.Abs(targetAngle - currentAngle) >= threshold)
+        {
+            float diff = targetAngle - currentAngle;
 
-        //character_animator = character_entity.GetComponent<Animator>();
-        //if (character_animator == null)
-        //    Debug.Log("null animator");
-        //else
-        //    Debug.Log(character_animator.name);
+            diff = Mathf.Repeat(diff + 180f, 360f) - 180f;
 
-        //StartCoroutine(Print());
+            if (Mathf.Abs(diff) < threshold)
+            {
+                currentAngle = targetAngle;
+            }
+            else
+            {
+                currentAngle += diff * rotateSpeed;
+            }
+
+
+            currentAngle = Mathf.Repeat(currentAngle, 360f);
+
+
+            //UnityEngine.Debug.Log($"当前角度: {currentAngle:F2}  |  目标角度: {targetAngle}");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //CalculateAnagle();
+        //RightRocker_H = Input.GetAxis("RightHorizontal");
+        //RightRocker_V = Input.GetAxis("RightVertical");
+        //this.currentDirection = this.transform.forward.normalized;
+
+        ////if (RightRocker_V == 0.0f && RightRocker_H == 0.0f)
+        ////{
+        ////    RightRocker_V = -1.0f;
+        ////}
+        //    this.rightController = new Vector3(RightRocker_H, 0.0f, -RightRocker_V).normalized;
+
+        //float dot = Vector3.Dot(new Vector3(0.0f, 0.0f, 1.0f), this.rightController);
+        //dot = Mathf.Clamp(dot, -1f, 1f); ;
+
+        ////float angle = 0.0f;
+        //if (RightRocker_H > 0.0f)
+        //{
+        //    targetAngle = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        //}
+        //else
+        //{
+        //    targetAngle = (Mathf.PI * 2 - Mathf.Acos(dot)) * Mathf.Rad2Deg;
+        //}
+
+        //this.transform.rotation = Quaternion.Euler(0, currentAngle, 0);
+
         RightRocker_H = Input.GetAxis("RightHorizontal");
         RightRocker_V = Input.GetAxis("RightVertical");
-        this.currentDirection = this.transform.forward.normalized;
-
-        if (RightRocker_V == 0.0f && RightRocker_H ==0.0f)
-        {
-            RightRocker_V = -1.0f;
-        }
-        this.rightController = new Vector3(RightRocker_H, 0.0f, -RightRocker_V).normalized;
-
-        float dot = Vector3.Dot(new Vector3(0.0f, 0.0f, 1.0f), this.rightController);
-        dot = Mathf.Clamp(dot, -1f, 1f); ;
-
-        float angle = 0.0f;
-        if (RightRocker_H > 0.0f)
-        {
-            angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
-        }
-        else
-        {
-            angle = (Mathf.PI * 2 - Mathf.Acos(dot)) * Mathf.Rad2Deg;
-        }
-
-        //Mathf.Lerp()
-        this.transform.rotation = Quaternion.Euler(0, angle, 0);
-        UnityEngine.Debug.Log(this.currentDirection + " : " + rightController + " : " + dot + " : " + angle);
 
         buttonPressed = Input.GetKey(KeyCode.JoystickButton5);
         if (buttonPressed && !startCheck)
@@ -101,7 +126,28 @@ public class Controller : MonoBehaviour
             //if (statInfo.IsName("Attack_03"))
             character_animator.Play("Idle");
         }
+        if (!buttonPressed)
+        {
+            float deadZone = 0.1f;
 
+            if (Mathf.Abs(RightRocker_H) > deadZone || Mathf.Abs(RightRocker_V) > deadZone)
+            {
+                this.rightController = new Vector3(RightRocker_H, 0.0f, -RightRocker_V).normalized;
+
+                float dot = Vector3.Dot(Vector3.forward, this.rightController);
+                dot = Mathf.Clamp(dot, -1f, 1f);
+
+                if (RightRocker_H > 0.0f)
+                    targetAngle = Mathf.Acos(dot) * Mathf.Rad2Deg;
+                else
+                    targetAngle = (Mathf.PI * 2 - Mathf.Acos(dot)) * Mathf.Rad2Deg;
+            }
+
+            CalculateAnagle();
+
+            this.transform.rotation = Quaternion.Euler(0, currentAngle, 0);
+
+        }
     }
 
     IEnumerator Print()
